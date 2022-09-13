f79870141adecb0104f5144d202f5346df8ef2dc
Adrian.Swoboda <adrian.swoboda93@gmail.com>
2022-09-13 10:40:41 +0200

    update template

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    'G:\Python\Kodilla\workspace\portfolio\infos\static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
